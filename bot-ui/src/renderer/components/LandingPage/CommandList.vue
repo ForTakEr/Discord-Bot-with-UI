8d9f6a192dd6a51d2d9710e1d6248a491fb9b232
hindrekhan <hindrek.hannus@tptlive.ee>
2019-02-26 13:41:29 +0200

    Add SettingsButton and CommandList

---
+++ bot-ui/src/renderer/components/LandingPage/CommandList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <ul id="server-list">
+            <li v-for="command in coomands">
+                {{ command.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  module.exports = {
+    data: function () {
+      return {
+        items: [
+          { name: 'hello' }
+        ]
+      }
+    }
+
+  }
+</script>
---
+++ bot-ui/src/renderer/components/LandingPage/SettingsButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button v-on:click="settingsClick">Settings</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        electron: process.versions.electron,
+        name: this.$route.name,
+        node: process.versions.node,
+        path: this.$route.path,
+        platform: require('os').platform(),
+        vue: require('vue/package.json').version
+      }
+    },
+    methods: {
+      settingsClick: function (event) {
+        alert('Hello')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
