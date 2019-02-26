e7c6c661923f25d78de59d802b56300ad6480121
hindrekhan <hindrek.hannus@tptlive.ee>
2019-02-26 14:10:37 +0200

    Add CommandsView

--- bot-ui/src/renderer/components/LandingPage.vue
+++ bot-ui/src/renderer/components/LandingPage.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="wrapper">
-    
     <TopBar></TopBar>
+    <CommandsView></CommandsView>
   </div>
 </template>
 
 <script>
   import TopBar from './LandingPage/TopBar'
+  import CommandsView from './LandingPage/CommandsView'
 
   export default {
     name: 'landing-page',
-    components: { TopBar }
+    components: { TopBar, CommandsView }
   }
 </script>
 
---
+++ bot-ui/src/renderer/components/LandingPage/CommandsView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <ul id="command-list">
+            <li v-for="command in commands">
+              <input type="checkbox">
+                {{ command.desc }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  module.exports = {
+    data: function () {
+      return {
+        commands: [
+          {
+            name: 'stream',
+            desc: 'Stream music from youtube',
+            enabled: false
+          },
+          {
+            name: 'meme',
+            desc: 'Send random meme',
+            enabled: true
+          }
+        ]
+      }
+    }
+
+  }
+</script>
+
