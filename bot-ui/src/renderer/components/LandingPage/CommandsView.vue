5b3af07587ba03208bd248fb8bd2b25c2f3a6d9c
jaanjahimees <>
2019-03-05 13:07:46 +0200

    Change command.enabled boolean on click

--- bot-ui/src/renderer/components/LandingPage/CommandsView.vue
+++ bot-ui/src/renderer/components/LandingPage/CommandsView.vue
@@ -2,7 +2,7 @@
     <div>
         <ul id="command-list">
             <li v-for="command in commands">
-              <input type="checkbox">
+              <input v-model="command.enabled" v-on:click="commandEnabled(command)" type="checkbox">
                 {{ command.desc }}
             </li>
         </ul>
@@ -15,16 +15,26 @@
       return {
         commands: [
           {
+            id: 0,
             name: 'stream',
             desc: 'Stream music from youtube',
             enabled: false
           },
           {
+            id: 1,
             name: 'meme',
             desc: 'Send random meme',
             enabled: true
           }
         ]
+      }
+    },
+    methods: {
+      // Change boolean of the command.
+      commandEnabled (command) {
+        let app = this
+        app.loading = true
+        command.enabled = !command.enabled
       }
     }
 
