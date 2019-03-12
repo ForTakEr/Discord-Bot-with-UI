48dc4ef0d1348d054cc4ff8d43a402c6de99ff8d
patrickteivas <teivas.2011@mail.ru>
2019-03-12 14:02:54 +0200

    wdq

--- bot/commands/meme.js
+++ bot/commands/meme.js
@@ -1,14 +1,22 @@
+const request = require('request');
+
+
 module.exports = async (msg) => {
     if (msg.content !== 'meme') {
         return false;
     }
 
-    const rando_imgs = [
-        //Insert meme links here.
-        "http://qnimate.com/wp-content/uploads/2014/03/images2.jpg"
-    ];
+    //https://some-random-api.ml/meme
+
+    var linkToMeme = "";
+
+    request('https://some-random-api.ml/meme', (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            linkToMeme = JSON.parse(body).url;
+        }
+    });
 
     return {
-        file: rando_imgs[Math.floor(Math.random() * rando_imgs.length)]
+        file: linkToMeme
     };
 };
