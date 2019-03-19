ba40f1b594c0da1597326da07273c4c2094e8278
hindrekhan <hindrek.hannus@tptlive.ee>
2019-03-19 14:14:22 +0200

    Add botbutton(wip)

---
+++ bot-ui/src/renderer/components/LandingPage/BotButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="bot-button-wrapper">
+    <button value="asdf" id="bot-button"></button>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+#bot-button-wrapper {
+  flex: 1;
+  background-color: aquamarine;
+  margin-top: 16px;
+  margin-left: 16px;
+}
+
+</style>
