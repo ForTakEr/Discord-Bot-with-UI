1111333d382a3f146ce3a0ec7793db1507c4a5cf
hindrekhan <hindrek.hannus@tptlive.ee>
2019-03-26 13:00:13 +0200

    Add bot switch button

--- bot-ui/src/renderer/components/LandingPage/BotButton.vue
+++ bot-ui/src/renderer/components/LandingPage/BotButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="bot-button-wrapper">
-    <button value="asdf" id="bot-button"></button>
+    <button v-on:click="botEnabledClick" type="checkbox" value="asdf" id="bot-button">{{ bot_button_text }}</button>
   </div>
 
 </template>
@@ -9,7 +9,19 @@
 export default {
   data () {
     return {
-
+      bot_button_text: 'Enabled'
+    }
+  },
+  methods: {
+    botEnabledClick: function () {
+      if (this.bot_button_text === 'Disabled') {
+        this.bot_button_text = 'Enabled'
+        return this.bot_button_text
+      }
+      if (this.bot_button_text === 'Enabled') {
+        this.bot_button_text = 'Disabled'
+        return this.bot_button_text
+      }
     }
   }
 }
@@ -18,9 +30,11 @@ export default {
 <style>
 #bot-button-wrapper {
   flex: 1;
-  background-color: aquamarine;
+  background-color: rgb(90, 209, 170);
   margin-top: 16px;
   margin-left: 16px;
+  width: 100%;
+  height: 100%;
 }
 
 </style>
